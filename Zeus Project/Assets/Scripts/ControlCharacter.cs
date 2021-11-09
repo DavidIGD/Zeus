ddc2411ed93d731b2d50a8ccf021b14bccb8eebe
DavidIGD <DavidIGD@outlook.com>
2021-11-09 06:14:23 +0000

    oh man

--- Zeus Project/Assets/Scripts/ControlCharacter.cs
+++ Zeus Project/Assets/Scripts/ControlCharacter.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class ControlCharacter : MonoBehaviour
 {
+    private Rigidbody2D speed;
+    private BoxCollider2D hitbox;
+    [SerializeField] private LayerMask ground;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        speed = GetComponent<Rigidbody2D>();
+        hitbox = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        speed.velocity = new Vector2(dirX * 7f, speed.velocity.y);
 
-        if(Input.GetKey("space"))
+        if(Input.GetButtonDown("Jump") && IsGrounded())
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed.velocity.x, 14f);
         }
 
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(hitbox.bounds.center, hitbox.bounds.size, 0f, Vector2.down, .1f, ground);
     }
 }
