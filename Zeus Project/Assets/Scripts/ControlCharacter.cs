4ce6eaf062c5d4027b65634869e33701ea24e0fc
DavidIGD <DavidIGD@outlook.com>
2021-11-09 05:25:40 +0000

    ahhhhhhhhhhhhhhhhhhhhhhh

--- Zeus Project/Assets/Scripts/ControlCharacter.cs
+++ Zeus Project/Assets/Scripts/ControlCharacter.cs
@@ -13,13 +13,11 @@ public class ControlCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
+
         if(Input.GetKey("space"))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
         }
-=======
-        
->>>>>>> Stashed changes
+
     }
 }
